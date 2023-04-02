009a8bab0820e78a1404ff0ea6b6e8fdda95307d
Niklas Johansson <canfightnj@gmail.com>
2023-04-02 15:27:33 +0200

    Add files via upload

--- canvasBG.js
+++ canvasBG.js
@@ -7,35 +7,37 @@ ctx.fillStyle = "#343434";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 var mX = canvas.width / 2;
 var mY = canvas.height / 2;
-onmousemove = function(e){mX=e.clientX;mY=e.clientY;}
+onmousemove = function (e) { mX = e.clientX; mY = e.clientY; }
 const src = document.getElementById("source");
-src.addEventListener(
-  "touchstart",
-  (e) => {
-    // Cache the client X/Y coordinates
-    mX = e.touches[0].clientX;
-    mY = e.touches[0].clientY;
-  },
-  false
-);
+if (src) {
+    src.addEventListener(
+        "touchstart",
+        (e) => {
+            // Cache the client X/Y coordinates
+            mX = e.touches[0].clientX;
+            mY = e.touches[0].clientY;
+        },
+        false
+    );
+}
 
 var pixels = [];
 for (let i = 0; i < 200; i++) {
-    pixels.push({px:Math.random() *canvas.width,py:Math.random() * canvas.height,c:"#ffffff"});
+    pixels.push({ px: Math.random() * canvas.width, py: Math.random() * canvas.height, c: "#ffffff" });
 }
-function updatePixel(p){
-    if(p.px < 0 || p.px > canvas.width || p.py < 0 || p.py > canvas.height){
+function updatePixel(p) {
+    if (p.px < 0 || p.px > canvas.width || p.py < 0 || p.py > canvas.height) {
         var angle = Math.random() * 7.28;
-        var rad = (150 + Math.random()*50);
+        var rad = (150 + Math.random() * 50);
         p.px = mX + rad * Math.cos(angle);
         p.py = mY + rad * Math.sin(angle);
-        p.c = "#" + Math.floor(Math.random()*10000000 + 6777215).toString(16);
+        p.c = "#" + Math.floor(Math.random() * 10000000 + 6777215).toString(16);
     }
-    var angle = Math.atan2(p.py - mY,p.px - mX);
+    var angle = Math.atan2(p.py - mY, p.px - mX);
     p.px = p.px + 4 * Math.cos(angle);
     p.py = p.py + 4 * Math.sin(angle);
     ctx.fillStyle = p.c;
-    ctx.fillRect(p.px, p.py, 5,5);
+    ctx.fillRect(p.px, p.py, 5, 5);
 }
 
 var intervalId = window.setInterval(function () {
