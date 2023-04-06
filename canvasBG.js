c2472d99bd11c4e1b1117fdbb0845845caab6ca8
Niklas Johansson <canfightnj@gmail.com>
2023-04-06 11:31:24 +0200

    Add files via upload

--- canvasBG.js
+++ canvasBG.js
@@ -23,21 +23,34 @@ if (src) {
 
 var pixels = [];
 for (let i = 0; i < 200; i++) {
-    pixels.push({ px: Math.random() * canvas.width, py: Math.random() * canvas.height, c: "#ffffff" });
+    pixels.push({ px: Math.random() * canvas.width, py: Math.random() * canvas.height,d:4,s:4, c: "#ffffff" });
 }
 function updatePixel(p) {
     if (p.px < 0 || p.px > canvas.width || p.py < 0 || p.py > canvas.height) {
         var angle = Math.random() * 7.28;
         var rad = (150 + Math.random() * 50);
+        p.d = 4;
+        p.s = 4;
         p.px = mX + rad * Math.cos(angle);
         p.py = mY + rad * Math.sin(angle);
         p.c = "#" + Math.floor(Math.random() * 10000000 + 6777215).toString(16);
     }
     var angle = Math.atan2(p.py - mY, p.px - mX);
-    p.px = p.px + 4 * Math.cos(angle);
-    p.py = p.py + 4 * Math.sin(angle);
+    if(p.d < 50){
+        p.d = p.d * 1.07;
+    }
+    if(p.s < 50){
+        p.s = p.s * 1.04;
+    }
+    p.px = p.px + p.s * Math.cos(angle);
+    p.py = p.py + p.s * Math.sin(angle);
     ctx.fillStyle = p.c;
     ctx.fillRect(p.px, p.py, 5, 5);
+    ctx.beginPath();
+    ctx.strokeStyle = p.c;
+    ctx.moveTo(p.px + 2, p.py + 2);
+    ctx.lineTo((p.px + (-p.d * Math.cos(angle))), (p.py + (-p.d * Math.sin(angle))));
+    ctx.stroke();
 }
 
 var intervalId = window.setInterval(function () {
@@ -46,4 +59,4 @@ var intervalId = window.setInterval(function () {
     pixels.forEach(p => updatePixel(p));
 
 
-}, 10);
+}, 17);
